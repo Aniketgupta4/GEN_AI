2a1002d9cf2892270199ec0234776a3702b24f1a
Aniketgupta4 <aniketkumargupta302@gmail.com>
2026-01-05 02:22:32 +0530

    genai

--- Chapter-6/script.js
+++ Chapter-6/script.js
@@ -24,6 +24,8 @@ import {exec} from "child_process";
 import util from "util"; // util ka use karray hai kyuki we use async await in function so without util callback hell problem ati hai so use -> util -> promisify -> jo function promise return karega usko async await ke sath use kar payenge
 const execute = util.promisify(exec);
 import os from "os"; // apan ko platform ke hisab se command dena hai na so os ka use karray hai 
+import { text } from "stream/consumers";
+
 
 const platform = os.platform(); // **** platform ka use karray hai ki konsa os use kar rahe hai user -> windows / linux / macos
 
@@ -69,12 +71,113 @@ const commandExecuter = {
 }
 
 
+
 const History = [];
+
 
 
 async function buildWebsite(){
 
     
+  while(true){
 
+    const result = await ai.models.generateContent({
+      model: "gemini-1.5-flash",
+      contents: History,
+      config:{
+        systemInstruction: `You are a website Builder, which will create the frontend part of the website using terminal/shell Command.
+         You will give shell/terminal command one by one and our tool will execute it.
+
+         Give the command according to the Operarting system we are using.
+         My Current user Operating system is: ${platform}.
+
+         Kindly use best practice for commands, it should handle multiline write also efficiently.
+
+         Your Job
+         1: Analyse the user query
+         2: Take the neccessary action after analysing the query by giving proper shell.command according to the user operating system.
+
+         Step By Step By Guide
+
+         1: First you have to create the folder for the website which we have to create, ex: mkdir calculator
+         2: Give shell/terminal command to create html file , ex: touch calculator/index.html
+         3: Give shell/terminal command to create CSS file 
+         4: Give shell/terminal command to create Javascript file 
+         5: Give shell/terminal command to write on html file 
+         6: Give shell/terminal command to write on css file 
+         7: Give shell/terminal command to write on javascript file
+         8: fix the error if they are persent at any step by writing, update or deleting`,
+        tools:[{
+          functionDeclarations: [commandExecuter],
+        }]
+      },
+
+    });
+
+     if(result.functionCalls && result.functionCalls.length>0){
+
+      const functionCall = result.functionCalls[0]; // **** jo function call hua hai usko le rahe hai
+
+      const {name,args} = functionCall;
+
+      const toolResponse = await executeCommand(args); // **** jo command execute karani hai usko yaha pe call kar rahe hai
+
+      const functionResponsePart = {
+            name: functionCall.name,
+            response: {
+                result: toolResponse,
+            },
+      };
+
+
+    // Send the function response back to the model.
+    History.push({
+      role: "model",
+      parts: [
+        {
+          functionCall: functionCall,
+        },
+      ],
+    });
+
+    History.push({
+      role: "user",
+      parts: [
+        {
+          functionResponse: functionResponsePart,
+        },
+      ],
+
+    });
+
+     }else{
+
+      console.log(result.text);
+      History.push({
+        role:"model",
+        parts:[{text:result.text}]
+
+      })
+     }
+  }
+}
+
+
+
+
+while(true){
+  
+  const question = readlineSync.question("Ask me anything --> ");
+
+  if(question==='exit'){
+    break;
+  }
+
+  History.push({
+    role:'user',
+    parts:[{text:question}]
+  });
+
+  await buildWebsite();
 
 }
