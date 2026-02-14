fe50c94615b95e88617297cb873502d7f30ce4ab
Aniketgupta4 <aniketkumargupta302@gmail.com>
2026-02-15 02:14:11 +0530

    genai

--- Chapter-16/script.js
+++ Chapter-16/script.js
@@ -95,13 +95,33 @@
 // -> **** AND ARRAY PE SIZE FIX HAI NODE KA 15 BYTES IN NEO4J --> SO AGAIN LINKEDLIST KO ARRAY PE CONVERT KARKE STORE IN DB --> IS TYPE KA KUCH CONCEPT
 
 
-// -> IMAGE8 -> **** HERE EACH INDEX IF OF 34-34 BYTES --> INDEXING IS FROM 0 AND IF WE KNOW BASE ADDRESS THEN WE FIND ANY ONE THIK
+
+//         //////NODE///////
+//        0// ROHIT       //  15 BYTES   
+//        1// MOHAN       //  15 BYTES              
+//        3// ..          //  15 BYTES               
+//        4// ..          //  15 BYTES              
+//        5// ..          //  15 BYTES              
+//        6// ..          //  15 BYTES               
+//        7// ..          //  15 BYTES               
+//        8// ..          //  15 BYTES              
+//        /////////////////                          
+//                                                  
+// --> **** THIS ARRAY METHOD FAILS DUE TO LESS SIZE 15 BYTES --> AND SUPPOSE 1020 LOCATION PE = 0:[1,2,3,6] STORE HAI 1040 PE = 1:[5,3,6,2] -----> SO AGAR NEW ELEMENT INSERT HONA HAI IN DONO MAI KISI MAI TOH NI KAR SHAKTE DUE TO ARRAY FIX SIZE SO THIS ------> FAILS        
+// ----> **** IMAGE9 --> YE ARRAY WALI APPROACH BEKAR HAI ISME PHIR SE ADDRESS FIX KARDENAIGAI SO --> ISSUE CREATE KAREGA IN REPLICA WAGERA PE -----> SO USE YE UPER WALI METHOD THIK
+
+
+
+
+
+// -> IMAGE8 -> NEW METHOD **** HERE EACH INDEX IF OF 34-34 BYTES --> INDEXING IS FROM 0 AND IF WE KNOW BASE ADDRESS THEN WE FIND ANY ONE THIK
 // **** ----> KYA HOGA INME EX: [ROHIT--FRIEND-->MOHIT  , 2(INDEX)] -> EACH DIBBE PE HOGA KON KISKA DOST HAI AND --> INDEX HOGA NEXT DIBBE KA KI ISME USER KE ANOTHER FRIEND HAI SAMJHE EX: [ROHIT--FRIEND-->SOHAN  , NULL(INDEX)] AND NULL MEANS HOGYA AB ITNA HI HAI
 
 // --> **** SO MAI SIRF INDEX STORE KARA RA HU --> AND RELATIONSHIP NIKALNA EASY HOGYA NA  
 
 
-// **** CONCLUSION ----> KI 1 ARRAY HAI THIK EACH NODE IS OF 15 BYTES WE ONLY STORE NAME OF USER AND THODA SA 15 BYTES SE MEMORY BACHI TOH FRIENDSHIP KA PURA DATA STORE NI KAR SHAKTE THIK --> SO KYA KARE FOR EACH NODE 1-1 ARRAY BANWA DIYE AND ISME EACH NODE SIZE IS 34 BYTES AND HM IS ARRAY MAI SPECIFIC USER RELATED FRIENDSHIP KARENGAI -> AND ISKE HAR EK NODE PE [FRIENDSHIP , INDEX] HOGA AND INDEX NEXT NODE KA HOGA JAHA US USER KE ANOTHER FRIEND KA RELLATION HOGA SAMJHE   
+// ----> **** RELATIONSHIP STORE --> IMPLEMENT ****ARRAY HAI --> MECHANISM ********WORKS/LOOKS LIKE LINKEDLIST[DUE TO INDEX STORE OF NEXT ONE] -> IMAGE10
+//  -> **** YAHA PE NODE BHI HOGA THIK SAME UPER WALI FUNCTIONS --> KI EACH INDEX OF NODE HAVE RELATIONSHIP THIK 
 
 //         //////NODE///////
 //        0// ROHIT       //  15 BYTES   -----------
@@ -127,10 +147,69 @@
 //                                         //////////////////////////                              /////////////////////
 //
 
+
 // --> **** AYSE HI HAR EK USER KE LIYE BANEGA THIK 
 
-// --> ********* EK OR ARRAY HOGA FOR SAME USER KNOWN AS ------> PROPERTY JISME PROPERTY HONGI OF THAT USER --> [NAME:ROHIT,INDEX:2] SO INDEX 2 PE KUCH OR PROPERTY HONGI [AGE:24,NULL] IS TYPE SE
+// --> ********* EK OR ARRAY HOGA FOR SAME USER KNOWN AS ------> PROPERTY[KEY:VALUE PAIR PE HOTI HAI] JISME PROPERTY HONGI OF THAT USER --> [NAME:ROHIT,INDEX:2] SO INDEX 2 PE KUCH OR PROPERTY HONGI [AGE:24,NULL] IS TYPE SE
+
+// **** -> SUPPOSE NEW DATA INSERT KARNA HAI INSERT IN RELATOSHIP ARRAY EASILY --> BUT ORIGINAL CONNECTION  BANANA PADEGA NA --> SO USME 1-1 BOHOT TRAVERSE KARNA HOGA SO EFFECTIVE WAY KI KHALI LAST INDEX PE INSERT HUA HAI PER USKO ORIGINAL START PE HI POINT KARA DOAND BATA DO ROOT KO KI 1ST INDEX AB 70 HAI SEE IN IMAGE --> IN O(1) TIME --> IMAGE11 12
+// --> MOTA MOTA KAHI BHI MEMORY DEDO AND INSERT IT IN START SO O(1) TC HOGI --> SEE IN LINKEDLIST LIKE DIAGRAM PE -> AND IF WE INSERT AT LAST SO O(N) TIME LAGTA SO START PE HI INSERT KAR DETE HAI THIK
+// --> IMAGE13 -> RELATIONSHIP ALSO LOOKS LIKE LINKEDLIST BUT ACTUAL ARRAY USE HORA HAI
+
+// ----> **** FOR EACH RELATIONSHIP THEIR IS PROPERTIES WALA ARRAY HOTA HAI --> STORE HOTA INFO KI WO DONO KAB CONNECT HUE DATE AND ALL
 
 
-// ----> **** IMAGE9 --> YE LINKEDLIST WALI APPROACH BEKAR HAI ISME PHIR SE ADDRESS FIX KARDENAIGAI SO --> ISSUE CREATE KAREGA IN REPLICA WAGERA PE -----> SO USE YE UPER WALI METHOD THIK
+
+// --> **** LET KISI NE ACCOUNT DELETE KAR DIYA TOH INDEX KHALI HO JAYEGA --> SO INDEX KO KHALI REHNE DO --> SHIFT WAGERA NI KARNA THIK NA
+// --> **** NOE4J MAINTAINS KI KONSA INDEX KHALI HAI AND JAB NEW DATA ATA HAI SO WO IS INDEX PE POINT KARA DETA HAI
+
+
+// --> ***** IMAGE14 OR INDEPTH PE ---> FOR EACH RELATION SABKE AGE , DOB , TIME , DATE HONGAI --> SO YE HAR USER KE LIYE HONGAI --> AND KEY:VALUE PAIR PE HOTA HAI NA ---> SO OR OPTIMIZE KARNE KE LIYE EK ARRAY/TABLE BANAKE INDEX ALLOCATE KAR DETE HAI SO AB PURA NI LIKHEGAI NAME AGE --> INDEX LIKHEGAI --> SPACE OPTIMIZE HUI NA
+
+
+
+
+// Q1 -> **** KUCH AYSA KI ROHIT TOH INKO FOLLOW KARTA HAI PER YE BHI PATA LAGE KI SOHAN/.... KO KON KON FOLLOW KARTA HAI YE INFO KAISE PATA LAGEGA ?
+// -> **** STORE THIS INFO TOO IN RELATIONSHIP BUT HOW ?
+
+// -> **** OR OPTIMIZE KAR DIYE KI **** NODE PE 2 CHIZE HONGI FIRST POINTER RELATIOSHIP AND SECOND POINTER RELATIONSHIP -> FIRST RELATIONSHIP NICHE DIAGRAM WALA AND SECOND RELATIONSIP PE STORE KAREGA --> KI ISKO KON KON FOLLOW KARTA HAI -> IMAGE15
+// -> **** HAR EK USER KE LIYE 2ND RELATIONSHIP HOGA JISPE HOGA KON KON FOLLOW KARTA HAI THIK
+
+
+//         //////NODE///////
+//        0// ROHIT       //  15 BYTES------------------------------------------>  //REATIONSHIP2//
+//        1// MOHAN       //  15 BYTES                POINTER2                     // SOHAN  // 
+//        3// ..          //  15 BYTES--------------                               // MOHAN  // 
+//        4// ..          //  15 BYTES              |                              // .....  // 
+//        5// ..          //  15 BYTES              |                              ////////////
+//        6// ..          //  15 BYTES              | 
+//        7// ..          //  15 BYTES              | POINTER1
+//        8// ..          //  15 BYTES              |
+//        /////////////////                         | 
+//                                                  |
+//                                                  \/
+//                                          //////////RELATIONSIO1/////                            ///////PROPERTY/////         
+//                                         0// ROHIT -> SOHAN , 1    //  34 BYTES                0 // NAME:ROHIT , 2 //
+//                                         1// ROHIT -> ARTI , 5     //  34 BYTES                1 //                //
+//                                         2// ..                   //  34 BYTES                 2 // AGE:24 , NULL  //  
+//                                         3// ..                   //  34 BYTES     ----->      3 //                 //        
+//                                         4// ..                    //  34 BYTES                4 //                 //
+//                                         5// ROHIT -> KOMAL , NULL //  34 BYTES                5 //                 //
+//                                         6// ..                   //  34 BYTES                 6 //                 //
+//                                         7// ..                   //  34 BYTES                 7 //                 //
+//                                         //////////////////////////                              /////////////////////
+//
+
+
+// --> **** 15 BYTES WALE PE 2 RELATIOSHIP PRESENT HOTA HAI SAMJHE
+
+
+
+// --> **** NEO4J NOT USES HASHMAP --> DUE TO SIZE AND ALL
+// --> **** FOR RANGE QUERIES NEO4J USES B+ TREES [HANDLES EFFICIENTLY IN BEST TIME COMPLEXITY]
+
+// -------------------------------------------- PART-2 --------------------------------------------------------
+
+
+// REVISION 
 
